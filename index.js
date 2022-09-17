b389b57fe07026e78ce23d4e2f550c320c2db992
Temiloluwa <temiloluwaogunti8@gmail,com>
2022-09-17 15:57:04 +0100

    redesigned the mobile hamburger toggle

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
 nav = document.querySelector('nav');
 navUl = document.querySelector('nav ul');
 hamburger = document.getElementById('hamburger');
 closeBtn = document.getElementById('close-btn');
+const body = document.querySelector('body');
+const bgToggleColor = document.querySelector('.bg-head-color')
 
 
 hamburger.addEventListener('click', (e)=>{
@@ -10,6 +11,8 @@ hamburger.addEventListener('click', (e)=>{
     // nav.style.backgroundColor = "black";
     hamburger.classList.add('show');
     closeBtn.classList.add('show')
+    body.classList.add('show');
+    bgToggleColor.classList.add("show");
 });
 
 
@@ -18,4 +21,5 @@ closeBtn.addEventListener('click', ()=>{
     nav.style.backgroundColor = "transparent";
     hamburger.classList.remove('show');
     closeBtn.classList.remove('show')
-})
+    bgToggleColor.classList.remove("show");
+});
